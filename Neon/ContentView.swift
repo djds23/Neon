0b294da8f6fec1c845b7b9f316e179bb836eb8f6
Dean Silfen <dean.silfen@gmail.com>
2019-06-08 16:44:43 -0400

    Initial Commit

---
+++ Neon/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Neon
+//
+//  Created by Dean Silfen on 6/8/19.
+//  Copyright © 2019 Dean Silfen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
